0b1a27c162b20cf78c9e05d8bde5e36396fd5cd1
RajWalke <rajwalke66@gmail.com>
2025-01-06 22:35:34 +0530

    Poster image is upadated and remove overview..

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,17 +9,12 @@ import TiltleInfo from "./TiltleInfo";
 const MainComponent=()=>{
     const movieFirst=useSelector((store)=>store.movie?.movieList);
     if(movieFirst === null) return;
-    const mainMovie=movieFirst[0];
-    const {id,original_title,overview}=mainMovie;
-    // const {id,original_title,overview}=mainmovie;
-    // console.log("mainmovie",mainmovie)
-    // console.log("id",id);
-    // console.log("original_title",original_title);
-    // console.log("overview",overview);
-    
+    const mainMovie=movieFirst[6];
+    const {id,original_title,overview,poster_path}=mainMovie;
+
     return(
     <div className="pt-52 px-14">
-        <TiltleInfo title={original_title} overview={overview}/>
+        <TiltleInfo title={original_title} posterPath={poster_path} overview={overview}/>
         <Tailor id={id}/>
     </div>
     );
--- src/components/TiltleInfo.js
+++ src/components/TiltleInfo.js
@@ -1,11 +1,13 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
-const TiltleInfo=({title,overview})=>{
+const TiltleInfo=({title,posterPath})=>{
     return (
-        <div className="">
+        <div className="flex flex-col items-center align-middle w-1/4">
+            
+            <img className="w-48 h-48 rounded-full my-4" src={"https://image.tmdb.org/t/p/w500/"+posterPath} alt="Poster_of movie"/>
             <h1 className="text-5xl font-bold">{title}</h1>
-            <p className="px-1 py-4 w-1/4 text-base text-start">{overview}</p>
-            <div className="flex justify-start cursor-pointer">
+            {/* <p className="px-1 py-4 w-1/4 text-base text-start">{overview}</p> */}
+            <div className="flex justify-start cursor-pointer ">
             <p className="text-2xl py-2 px-6 rounded-md text-white font-semibold bg-slate-400 m-2">▷ Play</p>
             <p className="text-2xl py-2 px-6 rounded-md text-white font-semibold bg-slate-400 m-2"><span className="font-thin">ⓘ</span> More Info</p>
             </div>
@@ -14,3 +16,5 @@ const TiltleInfo=({title,overview})=>{
     )
 }
 export default TiltleInfo;
+
+// image url=https://image.tmdb.org/t/p/w500/euYIwmwkmz95mnXvufEmbL6ovhZ.jpg
